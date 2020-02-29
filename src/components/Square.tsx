ea213151f52626d07491a3cc89456fdbe7b081b6
Maarten Claes <maartencls@gmail.com>
2020-02-29 22:47:38 +0100

    Improve styling of board and pieces

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 import * as React from "react";
 import { css, jsx } from '@emotion/core'
-import { Piece } from "../helpers"
+import { Piece, PieceColor } from "../helpers"
 
 export interface CellProps { 
     piece?: Piece, 
@@ -19,8 +19,8 @@ const cellBaseCSS = css`
   height: ${cellSize}px;
   line-height: ${cellSize}px;
   float: left;
-  background-color: darkgreen;
   font-size: ${cellSize - 5}px;
+  text-shadow: 2px 2px rgba(0, 0, 0, 0.1);
 
   font-weight: bold;
   text-align: center;
@@ -34,7 +34,7 @@ const cellBaseCSS = css`
 
 const darkCellCSS = css`
   ${cellBaseCSS};
-  background-color: darkgreen;
+  background-color: #3A3;
 `
 const lightCellCSS = css`
   ${cellBaseCSS};
@@ -89,7 +89,12 @@ export class Square extends React.Component<CellProps,{}> {
 
     render() {
         let content = this.props.piece != null ? this.props.piece.toEmoji() : ''
-        return <div css={this.isDarkCell ? darkCellCSS : lightCellCSS} 
+        let whitePiece = this.props.piece && this.props.piece.color == PieceColor.White
+        let style = css`
+            ${this.isDarkCell ? darkCellCSS : lightCellCSS}
+            color: ${whitePiece ? '#fff' : '#111'}
+        `
+        return <div css={style}
             title={this.position}
             onClick={this.handleClick.bind(this)}>
             {content}
